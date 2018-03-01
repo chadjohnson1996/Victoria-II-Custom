6e9e4a1f5a0ffa535023e1c98e2e464ba3514d1b
Chad Johnson <cjohnson@foundationsoft.com>
2018-03-01 11:23:44 -0500

    added loaders

--- Victoria II Custom Lib/FileLoader/Loaders/GameStateLoaders.cs
+++ Victoria II Custom Lib/FileLoader/Loaders/GameStateLoaders.cs
@@ -10,11 +10,6 @@ namespace Victoria_II_Custom_Lib.FileLoader.Loaders
     public class GameStateLoaders
     {
         /// <summary>
-        /// the issue loader
-        /// </summary>
-        public GameFileLoader IssueLoader { get; } = new GameFileLoader("common/issues.txt");
-
-        /// <summary>
         /// the decisions loader
         /// </summary>
         public GameFolderLoader DecisionsLoader { get; } = new GameFolderLoader("decisions", 2);
@@ -49,6 +44,10 @@ namespace Victoria_II_Custom_Lib.FileLoader.Loaders
         /// </summary>
         public GameFolderLoader UnitLoader { get; } = new GameFolderLoader("units");
 
+        #region History
+        /// <summary>
+        /// the diplomacy loader
+        /// </summary>
         public GameFolderLoader DiplomacyLoader { get; } = new GameFolderLoader("history/diplomacy");
 
         /// <summary>
@@ -60,7 +59,7 @@ namespace Victoria_II_Custom_Lib.FileLoader.Loaders
                 var index = name.IndexOf(" ");
                 var key = name.Substring(0, index + 1).Trim();
                 return key;
-            }, 
+            },
             true);
 
         /// <summary>
@@ -82,7 +81,9 @@ namespace Victoria_II_Custom_Lib.FileLoader.Loaders
             var key = Path.Combine(dirKey, fileName);
             return key;
         }, true);
+        #endregion
 
+        #region common
         /// <summary>
         /// loads the bookmarks
         /// </summary>
@@ -103,6 +104,118 @@ namespace Victoria_II_Custom_Lib.FileLoader.Loaders
         /// </summary>
         public GameFileLoader CotColorLoader { get; } = new GameFileLoader("common/cot_colors.txt");
 
+        /// <summary>
+        /// the country loader
+        /// </summary>
+        public GameFileLoader CountryLoader { get; } = new GameFileLoader("common/countries.txt");
+
+        /// <summary>
+        /// the country colors loader
+        /// </summary>
+        public GameFileLoader CountryColorsLoader { get; } = new GameFileLoader("common/country_colors.txt");
+
+        /// <summary>
+        /// the crime loader
+        /// </summary>
+        public GameFileLoader CrimeLoader { get; } = new GameFileLoader("common/crime.txt");
+        
+        /// <summary>
+        /// the culture loader
+        /// </summary>
+        public GameFileLoader CultureLoader { get; } = new GameFileLoader("common/cultures.txt");
+
+        /// <summary>
+        /// the defines loader
+        /// </summary>
+        public GameFileLoader DefinesLoader { get; } = new GameFileLoader("common/defines.lua");
+
+        /// <summary>
+        /// event modifiers loader
+        /// </summary>
+        public GameFileLoader EventModifiersLoader { get; } = new GameFileLoader("common/event_modifiers.txt");
+
+
+        /// <summary>
+        /// the goods loader
+        /// </summary>
+        public GameFileLoader GoodsLoader { get; } = new GameFileLoader("common/goods.txt");
+        
+        /// <summary>
+        /// the governments loader
+        /// </summary>
+        public GameFileLoader GovernmentsLoader { get; } = new GameFileLoader("common/governments.txt");
+
+        /// <summary>
+        /// the graphical culture loader
+        /// </summary>
+        public GameFileLoader GraphicalCultureLoader { get; } = new GameFileLoader("common/graphicalculturetype.txt");
+
+        /// <summary>
+        /// the ideologies loader
+        /// </summary>
+        public GameFileLoader IdeologyLoader { get; } = new GameFileLoader("common/ideologies.txt");
+        /// <summary>
+        /// the issue loader
+        /// </summary>
+        public GameFileLoader IssueLoader { get; } = new GameFileLoader("common/issues.txt");
+
+        /// <summary>
+        /// national focus
+        /// </summary>
+        public GameFileLoader NationalFocusLoader { get; } = new GameFileLoader("common/national_focus.txt");
+
+        /// <summary>
+        /// the national values loader
+        /// </summary>
+        public GameFileLoader NationalValueLoader { get; } = new GameFileLoader("common/nationalvalues.txt");
+
+        /// <summary>
+        /// the on actions loader
+        /// </summary>
+        public GameFileLoader OnActionsLoader { get; } = new GameFileLoader("common/on_actions.txt");
+
+        /// <summary>
+        /// the pop types loader
+        /// </summary>
+        public GameFileLoader PopTypeGlobalPromotionLoader { get; } = new GameFileLoader("common/pop_types.txt");
+        
+        /// <summary>
+        /// the production type loader
+        /// </summary>
+        public GameFileLoader ProductionTypeLoader { get; } = new GameFileLoader("common/production_types.txt");
+
+        /// <summary>
+        /// the rebel types loader
+        /// </summary>
+        public GameFileLoader RebelTypeLoader { get; } = new GameFileLoader("common/rebel_types.txt");
+
+        /// <summary>
+        /// the religion loader
+        /// </summary>
+        public GameFileLoader ReligionLoader { get; } = new GameFileLoader("common/religion.txt");
+
+        /// <summary>
+        /// the static modifiers loader
+        /// </summary>
+        public GameFileLoader StaticModifiersLoader { get; } = new GameFileLoader("common/static_modifiers.txt");
+
+        /// <summary>
+        /// the tech school loader
+        /// </summary>
+        public GameFileLoader TechSchoolLoader { get; } = new GameFileLoader("common/technology.txt");
+
+        /// <summary>
+        /// the traits loader
+        /// </summary>
+        public GameFileLoader TraitsLoader { get; } = new GameFileLoader("common/technology.txt");
+
+        /// <summary>
+        /// the triggered modifiers loader
+        /// </summary>
+        public GameFileLoader TriggeredModifiersLoader { get; } = new GameFileLoader("common/triggered_modifiers.txt");
+        #endregion
+
+
         public async Task Load()
         {
             await Task.WhenAll(IssueLoader.Load(), 
@@ -119,7 +232,10 @@ namespace Victoria_II_Custom_Lib.FileLoader.Loaders
                 BookmarkLoader.Load(),
                 BuildingLoader.Load(),
                 CbLoader.Load(),
-                CotColorLoader.Load()
+                CotColorLoader.Load(),
+                NationalFocusLoader.Load(),
+                GraphicalCultureLoader.Load(),
+                IdeologyLoader.Load()
                 );
         }
     }
