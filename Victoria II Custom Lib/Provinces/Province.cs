bac3479306cc2a246ca72507897063e39d3a7eff
Chad Johnson <cjohnson@foundationsoft.com>
2018-05-22 15:42:19 -0400

    added province class

--- Victoria II Custom Lib/Provinces/Province.cs
+++ Victoria II Custom Lib/Provinces/Province.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Victoria_II_Custom_Lib.Countries;
+using Victoria_II_Custom_Lib.Goods;
 
 namespace Victoria_II_Custom_Lib.Provinces
 {
@@ -11,5 +13,15 @@ namespace Victoria_II_Custom_Lib.Provinces
         public decimal Consciousness { get; set; }
 
         public decimal Militancy { get; set; }
+
+        public Country Owner { get; set; }
+
+        public Country Controller { get; set; }
+
+        public Dictionary<string, Country> Cores { get; set; } = new Dictionary<string, Country>();
+
+        public Good TradeGood { get; set; }
+
+        public int LifeRating { get; set; }
     }
 }
